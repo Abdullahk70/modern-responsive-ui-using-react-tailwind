c0f2ec99311c38ac98f8d338f871495356e3e990
Abdullahk70 <scientificgenius701924@gmail.com>
2023-11-12 12:39:50 +0500

    creating basic structure of the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-
+import styles from "./style"
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-300">
-    Hello world!
-  </h1>
+    <div className="bg-black w-full text-white overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+             Navbar
+        </div>
+      </div>
+
+
+      <div className={`bg-black ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+              hero
+        </div>
+      </div>
+
+      <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+              stats
+              Business
+              billing
+              carddeal
+              Testimonials
+              Clients
+              Cta
+              Foooter
+              
+        </div>
+      </div>
+
+
+  </div>
+
+
+
   );
 }
 
