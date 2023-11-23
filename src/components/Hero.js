997684bee5f7ab1309fb843ad658a4cc4d6e64a1
Abdullahk70 <scientificgenius701924@gmail.com>
2023-11-23 21:16:05 +0500

    basic work done on hero section. signing off for today!!

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,12 @@ import { discount,robot } from '../assets/assets'
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:p-0 sm:px-16 px-6`}></div>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:p-0 sm:px-16 px-6`}>
+        <div>
+          test hero
+        </div>
+      </div>
+
     </section>
   )
 }
