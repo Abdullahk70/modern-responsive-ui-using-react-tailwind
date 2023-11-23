e147cb4d3fed0b697a09761cf3e52f46ab088b9b
Abdullahk70 <scientificgenius701924@gmail.com>
2023-11-23 21:12:38 +0500

    started working on hero section of the app.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import styles from '../style'
+import { discount,robot } from '../assets/assets'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:p-0 sm:px-16 px-6`}></div>
+    </section>
   )
 }
 
